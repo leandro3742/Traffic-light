f9eda490b39e77d1408110853d3f02377708f988
leandro3742 <leandro.marrero@outlook.com>
2021-04-29 14:06:22 +0000

    semaforo con contador

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,63 +2,82 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	let rojoOff = "#CB4444";
-	let amarilloOff = "#D5DF37";
-	let verdeOff = "#37DF3C";
+	let rojoOff = "#FF6F6F";
+	let amarilloOff = "#FCFC6E";
+	let verdeOff = " #8AFC6E";
 
 	const [rojo, SetRojo] = useState(rojoOff);
 	const [amarillo, SetAmarillo] = useState(amarilloOff);
 	const [verde, SetVerde] = useState(verdeOff);
 
 	const botonRojo = {
-		width: "50px",
-		height: "50px",
-		backgroundColor: rojo,
-		borderRadius: "100%",
-		marginTop: "10px",
-		marginBottom: "10px"
+		backgroundColor: rojo
 	};
 
 	const botonAmarillo = {
-		width: "50px",
-		height: "50px",
-		backgroundColor: amarillo,
-		borderRadius: "100%",
-		marginTop: "10px",
-		marginBottom: "10px"
+		backgroundColor: amarillo
 	};
 
 	const botonVerde = {
-		width: "50px",
-		height: "50px",
-		backgroundColor: verde,
-		borderRadius: "100%",
-		marginTop: "10px",
-		marginBottom: "10px"
+		backgroundColor: verde
 	};
 
 	const prender_rojo = () => {
 		SetRojo("red");
 		SetAmarillo(amarilloOff);
 		SetVerde(verdeOff);
+		console.log(rojo);
+		if (rojo === rojoOff) {
+			console.log("LLega?");
+			setTimeout(() => {
+				console.log("Y aca?");
+				prender_verde();
+			}, 3000);
+		}
 	};
 	const prender_amarillo = () => {
 		SetAmarillo("yellow");
 		SetRojo(rojoOff);
 		SetVerde(verdeOff);
+		if (amarillo === amarilloOff) {
+			console.log("LLega?");
+			setTimeout(() => {
+				console.log("Y aca?");
+				prender_rojo();
+			}, 3000);
+		}
 	};
 	const prender_verde = () => {
 		SetVerde("green");
 		SetAmarillo(amarilloOff);
 		SetRojo(rojoOff);
+		if (verde === verdeOff) {
+			console.log("LLega?");
+			setTimeout(() => {
+				console.log("Y aca?");
+				prender_amarillo();
+			}, 3000);
+		}
 	};
 
 	return (
 		<div className="d-flex justify-content-center">
 			<div className="bg-dark col-1 mt-5 rounded d-flex flex-column align-items-center justify-content-center">
-				<button style={botonRojo} onClick={prender_rojo} />
-				<button style={botonAmarillo} onClick={prender_amarillo} />
-				<button style={botonVerde} onClick={prender_verde} />
+				<button
+					style={botonRojo}
+					onClick={prender_rojo}
+					className="botones"
+				/>
+				<button
+					style={botonAmarillo}
+					onClick={prender_amarillo}
+					className="botones"
+				/>
+				<button
+					style={botonVerde}
+					onClick={prender_verde}
+					className="botones"
+				/>
 			</div>
 		</div>
 	);
